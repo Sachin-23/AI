869f7a8f6f09329ee295495fd61474571b452965
Sachin-23 <Smudaliyar8452091824@gmail.com>
2020-10-30 11:02:29 +0530

    remove duplicate files

--- my/test.py
+++
@@ -1,8 +0,0 @@
-class InvalidMove(Exception):
-    """Base class for exceptions in this module."""
-    pass
-
-try:
-    raise InvalidMove("InvalidAction")
-except InvalidMove as e:
-    print(Action)
