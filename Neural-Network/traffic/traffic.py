45bfc2723f26debf47c8bbf11d20ac2960d94dce
Sachin-23 <Smudaliyar8452091824@gmail.com>
2021-06-05 18:01:51 +0530

    complete project: traffic

--- Neural-Network/traffic/traffic.py
+++ Neural-Network/traffic/traffic.py
@@ -102,7 +102,7 @@ def get_model():
 
             tf.keras.layers.Dense(256, activation="relu"),
 
-            tf.keras.layers.Dropout(0.5),
+            tf.keras.layers.Dropout(0.4),
 
             tf.keras.layers.Dense(NUM_CATEGORIES, activation="softmax")
         ])
